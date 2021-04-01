f2e7e97d352d43269c1d5800c7d71d23882a84a6
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-04-01 16:09:21 -0300

    add new class

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`Its current speed is${vel} Km/h`)
+if(vel > 60){ //simple condiction 
+    console.log(`You are FINE For speeding`)
+}
+console.log('lways drive with the seat belt!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('you are brasilian')
+}else{
+    console.log('are you a foreigner')
+}
