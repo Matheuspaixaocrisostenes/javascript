5979cd6d8dd08cbc14e0ea16170c127c37551233
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-04-19 16:06:42 -0300

    update ex

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,18 +1,28 @@
 function contar(){
-    var inicio = document.getElementById('txtn1').value
-    var fim = document.getElementById('txtn2').value
-    var passo = document.getElementById('txtp').value
+    var inicio = document.getElementById('txtn1')
+    var fim = document.getElementById('txtn2')
+    var passo = document.getElementById('txtp')
     var res = document.getElementById('result')
-    let i = Number(inicio)
-    let f = Number(fim)
-    let p = Number(passo)
-    res.innerText = ''
-    if(p==0){
-        alert('Passo Invalido, passo valendo 1')
-        p = 1
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerText = 'Impossivel de contar'
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p<=0){
+            alert('Passo Invalido conssiderando passo 1') 
+            p=1
+        }
+        if(i < f){
+            for(let c = i; c <= f; c+=p){
+                res.innerHTML += `${c} \u{1f449} `
+            }
+        }else{
+            for(let c = i; c >= f; c-=p){
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
-    for(var c = i; c <= f; c+=p){
-        res.innerText += c + `\u{1f449}`
-    }
-    res.innerText+= `\u{1f3c1}`
 }
