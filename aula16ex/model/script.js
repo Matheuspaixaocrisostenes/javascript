d6c5b8195833f9ac2159d4cd720fb81068de3cf5
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-04-27 16:28:00 -0300

    add new ex

---
+++ aula16ex/model/script.js
@@ -0,0 +1,28 @@
+let num = document.getElementById('fnum')
+let sel = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n,l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function add(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        
+    }else{
+        alert('Valor Invalido ou Já encontrado na Lista')
+    }
+}
